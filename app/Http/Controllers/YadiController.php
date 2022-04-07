1e436e9a794df622fb92dd98615149813d03ad7f
syahrul anwar <syahrulanwar83@gmail.com>
2022-04-07 17:28:42 +0800

    crud pertama

---
+++ app/Http/Controllers/YadiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Yadi;
+use Illuminate\Http\Request;
+
+class YadiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Yadi  $yadi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Yadi $yadi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Yadi  $yadi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Yadi $yadi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Yadi  $yadi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Yadi $yadi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Yadi  $yadi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Yadi $yadi)
+    {
+        //
+    }
+}
